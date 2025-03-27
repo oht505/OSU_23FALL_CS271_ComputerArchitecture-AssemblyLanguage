7c4bc2ab1ac64758072ed3dae882e6cdb2b38636
Hyuntaek Oh <56432035+oht505@users.noreply.github.com>
2025-03-26 18:49:31 -0700

    Add files via upload

---
+++ Project0/Template.asm
@@ -0,0 +1,29 @@
+TITLE Program Template     (template.asm)
+
+; Author: HyunTaek Oh
+; Last Modified: 
+; OSU email address: ohhyun@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number:                 Due Date:
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+
+.code
+main PROC
+
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
